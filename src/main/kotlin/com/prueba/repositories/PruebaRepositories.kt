cec380cd6e8857a6eccaf134e43350e165e80bf6
Oscar Arellano <arellanooscar.ing@gmail.com>
2025-01-16 17:08:27 -0500

    eliminate innecesary dependencies

--- src/main/kotlin/com/prueba/repositories/PruebaRepositories.kt
+++
@@ -1,9 +0,0 @@
-package com.prueba.repositories
-
-import com.prueba.models.Prueba
-import io.micronaut.data.annotation.Repository
-import io.micronaut.data.repository.CrudRepository
-
-@Repository
-interface PruebaRepositories: CrudRepository<Prueba, Long> {
-}
--- src/main/resources/db/migration/V1__create_table_example.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE TABLE pruebas (
-                         id SERIAL PRIMARY KEY,
-                         name VARCHAR(255) NOT NULL,
-                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
